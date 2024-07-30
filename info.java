a3c62d3dd53770d49953b810e72b2579123fc55a
Prasad-Upasani <prasadupasani771@gmail.com>
2024-07-30 22:08:03 +0530

    developer 1 created two files

---
+++ info.java
@@ -0,0 +1,11 @@
+public class info
+{
+ public static void main(String args[])
+{
+   System.out.println("Name:- Prasad Upasani");
+   System.out.println("Contact No:- 12345678");
+   System.out.println("E-mail:-prasad123@gmail.com");
+   System.out.println("City:-Dhule");
+System.out.println("Education:- BCA");
+}
+}
